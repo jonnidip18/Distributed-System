eef829feb18899caf681f61c8df7e7007e8b0d65
KenJonnidip <jonnidip18@gmail.com>
2021-05-20 10:02:17 +0700

    refactor

--- rmi_practice/Client.java
+++ rmi_practice/Client.java
@@ -1,10 +1,18 @@
 package rmi_practice;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+            Registry registry = LocateRegistry.getRegistry("127.0.0.1", 1099);
+            ServerService stub = (ServerService) registry.lookup("ServerService"); // same name when server register to RMI server
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 
 }
--- rmi_practice/ServerServiceImp.java
+++ rmi_practice/ServerServiceImp.java
@@ -1,22 +1,10 @@
 package rmi_practice;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
 
 public class ServerServiceImp {
-	ServerServiceImp obj = new ServerServiceImp();
-
-    // Create an stub to store in RMI server
-    try {
-        ServerService stub = (ServerService) UnicastRemoteObject.exportObject(obj, 1099);
-
-        //  Register stub to RMI server
-        Registry registry = LocateRegistry.createRegistry(1099);
-        registry.bind("ServerService", stub); // ServerSevice will be the name in RMI server when client will lookup
-        
-        System.out.println("Sever ready");
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
+	public ServerServiceImp() {}
+	
+	@Override
+	public 
 }
